3a7b91797ba172c7d58feb9ec284b1cf9410b330
lukemew <lucasmelor65@gmail.com>
2025-02-20 11:00:39 -0300

    Inserindo novo componente de navbar, iniciando a página de Anunciar Imóvel e Corrigindo logo para .png

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Cadastro from '../pages/Cadastro/Cadastro';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Cadastro from "../pages/CadastroUserPage/Cadastro";
+import CadastroImovel from "../pages/CadastroImovelPage/CadastroImovel";
 
 const AppRoutes = () => {
   return (
     <Router>
       <Routes>
         <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/cadastrar_imovel" element={<CadastroImovel />} />
         {/* Adicione uma rota padrão para redirecionar para /cadastro */}
         <Route path="/" element={<Cadastro />} />
       </Routes>
