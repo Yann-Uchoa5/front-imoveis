6ce10b8da0842eca62fc59a0c98fdce9273fc444
lukemew <lucasmelor65@gmail.com>
2025-02-22 13:35:01 -0300

    feat: inserindo ao componente FormInput a possibilidade de receber arquivos

--- src/components/FormInput/FormInput.js
+++ src/components/FormInput/FormInput.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import './FormInput.css';
+import React from "react";
+import "./FormInput.css";
 
 const FormInput = ({ type, placeholder, value, onChange, required }) => {
   return (
     <input
-      type={type}        // Agora o tipo é genérico
+      type={type} // Agora o tipo é genérico
       placeholder={placeholder}
-      value={value}      // O valor será o valor do estado passado para cada campo
+      value={value} // O valor será o valor do estado passado para cada campo
       onChange={onChange} // A função onChange vem do componente pai para atualizar o estado
       required={required}
-      className="form-input"
+      className={type == "file" ? "file-input" : "form-input"}
     />
   );
 };
