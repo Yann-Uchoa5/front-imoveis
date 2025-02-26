f42659b78504b15ec8e60b89ec517d16d2e273db
lukemew <lucasmelor65@gmail.com>
2025-02-26 12:27:30 -0300

    feat: finalizando a página Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,54 +1,62 @@
 import SearchBar from "../../components/SearchBar/SearchBar";
 import PropertyCard from "../../components/PropertyCard/PropertyCard";
 import "./Home.css";
-import casa1 from "../../assets/images/casa1.jpg";
-import casa2 from "../../assets/images/casa2.jpg";
-import apto1 from "../../assets/images/apto1.jpg";
-import casa3 from "../../assets/images/casa3.jpg";
+import properties from "../../assets/data/properties";
 import MainNavbar from "../../components/MainNavbar/MainNavbar";
 import Mural from "../../components/Mural/Mural";
+import Footer from "../../components/Footer/Footer";
 
 const Home = () => {
-  const properties = [
-    {
-      image: casa1,
-      title: "Casa em Salvador",
-      price: "140.000,00",
-      location: "Salvador - BA",
-    },
-    {
-      image: apto1,
-      title: "Apt em Quixadá",
-      price: "500,00",
-      location: "Quixadá - CE",
-    },
-    {
-      image: casa2,
-      title: "Casa em Boa Viagem",
-      price: "140.000,00",
-      location: "Boa Viagem - CE",
-    },
-    {
-      image: casa3,
-      title: "Casa em Henrique Jorge",
-      price: "350.000,00",
-      location: "Madalena - CE",
-    },
-  ];
+  const topProperties = properties.slice(0, 4);
+
+  //aqui é filtrado apenas as casas que ainda não apareceram na sessão principal
+  const remainingProperties = properties.filter(
+    (p) => !topProperties.includes(p)
+  );
+
+  //aqui é filtrado as casas disponíveis para aluguel
+  const rentProperties = remainingProperties
+    .filter((p) => p.type === "Locação")
+    .slice(0, 4);
+
+  //aqui é filtrado as casas disponíveis para compra
+  const buyProperties = remainingProperties
+    .filter((p) => p.type === "Compra")
+    .slice(0, 4);
 
   return (
-    <div>
+    <div className="home_container">
       <MainNavbar />
       <div className="hero">
         <SearchBar />
       </div>
       <h2>Imóveis que baixaram em até 36% de preço perto de você</h2>
       <div className="property-list">
-        {properties.map((p, index) => (
+        {topProperties.map((p, index) => (
           <PropertyCard key={index} {...p} />
         ))}
       </div>
       <Mural />
+      <h2>
+        Imóveis disponíveis para <b>COMPRA</b>
+      </h2>
+      <div className="property-list">
+        {buyProperties.map((p, index) => (
+          <PropertyCard key={index} {...p} />
+        ))}
+      </div>
+      <h2>
+        Imóveis disponíveis para <b>ALUGUEL</b>
+      </h2>
+      <div className="property-list">
+        {rentProperties.map((p, index) => (
+          <PropertyCard key={index} {...p} />
+        ))}
+      </div>
+      <h2>
+        Precisa de <a href="#ajuda">AJUDA?</a>
+      </h2>
+      <Footer />
     </div>
   );
 };
