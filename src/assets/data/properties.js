397a2eac6b747ec9f12b3fe3fb5684481079315c
lukemew <lucasmelor65@gmail.com>
2025-02-26 12:24:57 -0300

    feat: agora os dados das propriedades estão dinamicamente salvos no arquivo  properties.js

---
+++ src/assets/data/properties.js
@@ -0,0 +1,125 @@
+import casa1 from "../../assets/images/imoveis/casa1.jpg";
+import casa2 from "../../assets/images/imoveis/casa2.jpg";
+import apto1 from "../../assets/images/imoveis/apto1.jpg";
+import casa3 from "../../assets/images/imoveis/casa3.jpg";
+import apto2 from "../../assets/images/imoveis/apto2.png";
+import casa4 from "../../assets/images/imoveis/casa4.png";
+import apto3 from "../../assets/images/imoveis/apto3.png";
+import casa5 from "../../assets/images/imoveis/casa5.png";
+import apto4 from "../../assets/images/imoveis/apto4.png";
+import apto5 from "../../assets/images/imoveis/apto5.png";
+import casa6 from "../../assets/images/imoveis/casa6.png";
+import casa7 from "../../assets/images/imoveis/casa7.png";
+
+const properties = [
+  {
+    image: casa1,
+    title: "Casa em Salvador",
+    price: "140.000,00",
+    type: "Compra",
+    area: 32,
+    banheiros: 2,
+    quartos: 3,
+  },
+  {
+    image: apto1,
+    title: "Apt em Quixadá",
+    price: "500,00",
+    type: "Locação",
+    area: 10,
+    banheiros: 1,
+    quartos: 2,
+  },
+  {
+    image: casa2,
+    title: "Casa em Boa Viagem",
+    price: "140.000,00",
+    type: "Compra",
+    area: 22,
+    banheiros: 2,
+    quartos: 4,
+  },
+  {
+    image: casa3,
+    title: "Casa em Henrique Jorge",
+    price: "350.000,00",
+    type: "Compra",
+    area: 20,
+    banheiros: 1,
+    quartos: 3,
+  },
+  {
+    image: casa4,
+    title: "Casa em Boa Viagem CE",
+    price: "140.000,00",
+    type: "Compra",
+    area: 20,
+    banheiros: 1,
+    quartos: 3,
+  },
+  {
+    image: casa5,
+    title: "Casa em Madalena CE",
+    price: "140.000,00",
+    type: "Compra",
+    area: 20,
+    banheiros: 1,
+    quartos: 3,
+  },
+  {
+    image: apto2,
+    title: "Apt em Fortaleza CE",
+    price: "240.000,00",
+    type: "Compra",
+    area: 20,
+    banheiros: 1,
+    quartos: 3,
+  },
+  {
+    image: apto3,
+    title: "Apt em Sobral CE",
+    price: "250.000,00",
+    type: "Compra",
+    area: 20,
+    banheiros: 1,
+    quartos: 3,
+  },
+  {
+    image: casa6,
+    title: "Casa em Juazeiro do Norte",
+    price: "500,00",
+    type: "Locação",
+    area: 20,
+    banheiros: 1,
+    quartos: 3,
+  },
+  {
+    image: apto4,
+    title: "Apt em Tauá CE",
+    price: "350,00",
+    type: "Locação",
+    area: 20,
+    banheiros: 1,
+    quartos: 3,
+  },
+  {
+    image: casa7,
+    title: "Casa em Crato CE",
+    price: "750,00",
+    type: "Locação",
+    area: 20,
+    banheiros: 1,
+    quartos: 2,
+  },
+  {
+    image: apto5,
+    title: "Apt em Quixadá CE",
+    price: "380,00",
+    type: "Locação",
+    area: 10,
+    banheiros: 1,
+    quartos: 1,
+  },
+];
+
+export default properties;
