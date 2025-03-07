74dedf7ce692005fad1b378c1a77e136716dd7e3
Wesley00s <wesley300rodrigues@gmail.com>
2025-03-07 19:47:17 -0300

    feat: create action menu component

---
+++ src/components/ActionMenu/ActionMenu.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import "./ActionMenu.css";
+
+const ActionMenu = ({ imovel, onEdit, onDelete }) => {
+    const [menuAberto, setMenuAberto] = useState(false);
+
+    return (
+        <div
+            className="action-menu-container"
+            onMouseEnter={() => setMenuAberto(true)}
+            onMouseLeave={() => setMenuAberto(false)}
+        >
+            <button className="action-menu-button">
+                <i className="fas fa-ellipsis-v"></i>
+            </button>
+            {menuAberto && (
+                <div className="action-menu">
+                    <button onClick={() => onEdit(imovel)}>Editar</button>
+                    <button onClick={() => onDelete(imovel)}>Excluir</button>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default ActionMenu;
